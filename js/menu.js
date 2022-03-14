6c2524da87a685af094a919ba1495c24418397cf
Devdesiign <Devdesiign@gmail.com>
2022-03-14 18:05:40 +0100

    Mobile view Menu, 100%

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+//Open Menu
+function openmenu() {
+    document.getElementById("open-menu-icon").style.display = "none";
+    document.getElementById("close-menu-icon").style.display = "block";
+    document.getElementById("menu-board").style.display = "block";
+}
+//Close Menu
+function closemenu() {
+    document.getElementById("close-menu-icon").style.display = "none";
+    document.getElementById("open-menu-icon").style.display = "block";
+    document.getElementById("menu-board").style.display = "none";
+}
